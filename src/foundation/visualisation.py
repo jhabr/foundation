ad9dadc710254d1b713ec15cbb80f40121359687
Jaro Habr <jaro@freshbits.ch>
2023-01-28 15:04:47 +0100

    more refactorings

--- src/foundation/visualisation.py
+++ src/foundation/visualisation.py
@@ -24,22 +24,22 @@ def trace(root: Scalar) -> tuple:
 
 
 def draw_graph(root: Scalar) -> graphviz.Digraph:
-    dot = graphviz.Digraph(format="svg", graph_attr={"rankdir": "LR"})
+    graph = graphviz.Digraph(format="svg", graph_attr={"rankdir": "LR"})
     nodes, edges = trace(root=root)
 
     for node in nodes:
         uid = str(id(node))
-        dot.node(
+        graph.node(
             name=uid,
             label="{%s | data: %.4f | grad: %.4f}" % (node.label, node.data, node.grad),
             shape="record",
         )
 
         if node.operation:
-            dot.node(name=uid + node.operation, label=node.operation)
-            dot.edge(tail_name=uid + node.operation, head_name=uid)
+            graph.node(name=uid + node.operation, label=node.operation)
+            graph.edge(tail_name=uid + node.operation, head_name=uid)
 
     for node1, node2 in edges:
-        dot.edge(tail_name=str(id(node1)), head_name=str(id(node2)) + node2.operation)
+        graph.edge(tail_name=str(id(node1)), head_name=str(id(node2)) + node2.operation)
 
-    return dot
+    return graph
